0cb09b3af135c4a09e784f125d98a9b22eef6641
Danijel <susakovich@gmail.com>
2022-11-25 13:50:13 +0100

    Remove script for contact form

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-// let tabLinks = document.getElementsByClassName("about__tab-links");
-// let tabContents = document.getElementsByClassName("about__tab-contents");
-
-// function openTab(tabName) {
-//   for (tabLink of tabLinks) {
-//     tabLink.classList.remove("active-link");
-//   }
-//   for (tabContent of tabContents) {
-//     tabContent.classList.remove("active-tab");
-//   }
-//   event.currentTarget.classList.add("active-link");
-//   document.getElementById("tabName").classList.add("active-tab");
-// }
-
 let sideMenu = document.getElementById("sideMenu");
 
 function openMenu() {
@@ -21,23 +7,3 @@ function openMenu() {
 function closeMenu() {
   sideMenu.style.right = "-120px";
 }
-
-// Form Script
-
-const scriptURL =
-  "https://script.google.com/macros/s/AKfycby_Jo9z7dRS2qJVU1TYY07BShDLkTZx1pKlQ0QVJDXNRIYTEhbvw6HzN1RlwcQ6gq3cRg/exec";
-const form = document.forms["submit-to-google-sheet"];
-const msg = document.getElementById("msg");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then((response) => {
-      msg.innerHTML = "Message sent successfully";
-      setTimeout(function () {
-        msg.innerHTML = "";
-      }, 2000);
-      form.reset();
-    })
-    .catch((error) => console.error("Error!", error.message));
-});
