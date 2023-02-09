9cd9e9c9c6a9d9e4ddb4020069890efcd541a05b
Danijel <susakovich@gmail.com>
2023-02-09 15:03:03 +0100

    Add reset on form after submiting

--- main.js
+++ main.js
@@ -7,3 +7,21 @@ function openMenu() {
 function closeMenu() {
   sideMenu.style.right = "-120px";
 }
+
+// Get all input elements in the form
+let inputs = document.querySelectorAll("input, textarea");
+
+// Attach a submit event listener to the form
+document.querySelector("form").addEventListener("submit", function (event) {
+  // Prevent the form from submitting
+  event.preventDefault();
+
+  // Loop through all input elements
+  for (var i = 0; i < inputs.length; i++) {
+    // Clear the value of each input element
+    inputs[i].value = "";
+  }
+
+  // Log a message to confirm that the form was submitted
+  console.log("Form submitted!");
+});
